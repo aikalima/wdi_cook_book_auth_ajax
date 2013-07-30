909c9b666d7b06bebccbd60c33834aa39126be92
markusg <mguehrs@gmail.com>
2013-07-30 15:35:58 -0700

    init

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+CookBook::Application.routes.draw do
+  resources :books, :recipes, :ingredients, :users, :sessions, :comments
+
+  get '/signup' => 'users#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  get'/signin' => 'sessions#new'
+
+  root to: 'books#index'
+
+  get '/data.json' => 'stats#data'
+  get '/stats' => 'stats#stats'
+  get '/comments/:book_id' => 'comments#new'
+
+
+end
+
